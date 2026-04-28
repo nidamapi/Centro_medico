f333023a6ec796e254685583c1706266dbcadd41
nidamapi <nicollemacias@colegioguanenta.edu.co>
2026-04-28 09:29:30 -0500

    actualización

---
+++ config/conexion.php
@@ -0,0 +1,38 @@
+<?php
+    // script para crear una conexión con la BD
+
+    // Parametros requeridos para la conexión con la BD
+
+    // Parametros BD local - Constantes
+    define('USER', 'nmacias');// Crea la constante USER con valor 'root'
+    define('PW', '12345');
+    define('HOST', 'localhost');
+    define('BD', 'centro_medico');
+
+    // Parametros BD remota (infinityfree)
+    /*define('USER', 'if0_41712668'); // Crea la constante USER con valor 'root'
+    define('PW', '7rRJIxHMdKGo');
+    define('HOST', 'sql306.infinityfree.com');
+    define('BD', 'if0_41712668_centro_medico');*/
+
+    // Conexión con la BD
+    $conexion = mysqli_connect(HOST, USER, PW, BD);
+
+    // Establecer conjunto de caracteres para el hosting
+    mysqli_set_charset($conexion, 'utf8mb4');
+    
+    // Verificar la conexión con la BD
+
+    if (!$conexion)
+    {
+        die("La conexión con la BD falló: " + mysqli_error
+        ($conexion));
+    }
+    else
+    {
+        die("Conexión a la BD exitosa!");
+    }
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Centro Médico</title>
+</head>
+<body>
+	<h1>Centro Médico Nidamapi</h1>
+	<h2>Nicolle Daniela Macías Piracón</h2>
+</body>
+</html>
