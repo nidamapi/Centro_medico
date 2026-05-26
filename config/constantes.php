14aec6bc35ae5cf596a44979e5272ac9f8325ff3
nidamapi <nicollemacias@colegioguanenta.edu.co>
2026-05-26 06:16:14 -0500

    actualización

--- config/constantes.php
+++ config/constantes.php
@@ -1,16 +1,13 @@
 <?php
-    // Parametros requeridos para la conexión con la BD
-
     // Parametros BD local - Constantes
-    define('USER', 'nmacias');// Crea la constante USER con valor 'root'
+    define('USER', 'npaez'); //  Crea la constante USER con valor 'root'
     define('PW', '12345');
     define('HOST', 'localhost');
     define('BD', 'centro_medico');
 
     // Parametros BD remota (infinityfree)
-    /*define('USER', 'if0_41712668'); // Crea la constante USER con valor 'root'
-    define('PW', '7rRJIxHMdKGo');
-    define('HOST', 'sql306.infinityfree.com');
-    define('BD', 'if0_41712668_centro_medico');*/
-
+    /*define('USER', 'if0_41712633'); //  Crea la constante USER con valor 'root'
+    define('PW', '57CwSVfTrjcl');
+    define('HOST', 'sql213.infinityfree.com');
+    define('BD', 'if0_41712633_centro_medico');*/
 ?>
--- modelo/usuario_modelo.php
+++ modelo/usuario_modelo.php
@@ -1,34 +1,52 @@
 <?php
-    require_once "../config/conexion.php";
+    require (__DIR__."/../config/conexion.php");
     
-    // función para buscar un usuario por su login
+    // funcion para buscar un usuario por su login
     function validar_usuario($login, $password)
     {
-        // Estabecer conexión con la BD
+        // Establecer conexión con la BD
         $conexion = conectar();
 
+        echo "<br>función validar_usuario, del modelo, ejecutándose...";
+
         // Instruccion SQL para hacer la consulta a la BD
-        $sql= "SELECT login_usuario, password_usuario, 
-        estado_usuario, tipo_usuario, COUNT(*) AS contar FROM
-        Usuario WHERE login_usuario = $login AND password_usuario 
-        = $password'";
+        $sql = "SELECT id_usuario, login_usuario, password_usuario, estado_usuario, tipo_usuario, COUNT(*) AS contar FROM Usuario WHERE login_usuario = '$login' AND password_usuario = '$password'";
 
         // Ejecutar la consulta SQL a la BD
-        $consulta = mysqli_query($conexion, $sql) or trigger_error
-        ("Error en la consulta MySql: " + mysqli_error($conexion));
-        
-        // Convertir consulta en array
-        $resultado = mysqli_fetch_array($consulta);
+        $consulta = mysqli_query($conexion, $sql) or trigger_error("Error en la consulta MySql: ".mysqli_error($conexion));
 
-        // Verificar si el usuario existe en la BD
+        // Convertir consulta en array
+        //$resultado = mysqli_fetch_array($consulta);
+        $resultado = mysqli_fetch_assoc($consulta);
+
+        // verificar si el usuario existe en la BD
         if($resultado['contar']>0)
         {
-            echo 'El usuario existe en la BD';
-            echo $resultado['login_usuario'];
+            echo '<br>El usuario existe en la BD';
+            echo '<br>Id: '.$resultado['id_usuario'];
+            echo '<br>Usuario: '.$resultado['login_usuario'];
+            echo '<br>Tipo: '.$resultado['tipo_usuario'];
+            echo '<br>Estado: '.$resultado['estado_usuario'];
         }
         else
         {
-            echo 'El usuario exise en la BD, o login password incorrecto';
+            echo '<br>El usuario no existe, o login o password incorrecto';
         }
+
+        return $resultado;
     }
+
+    
+    // probar consulta a BD
+
+    $user = 'medi01';
+    $pass = 123;
+
+    echo 'Probando consulta...';
+    echo '<br>'.$user;
+    echo '<br>'.$pass;
+    
+
+    validar_usuario($user, $pass);
+
 ?>
